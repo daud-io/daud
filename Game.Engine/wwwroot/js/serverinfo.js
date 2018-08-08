d583f52dba1dc1e6ad08793248b8cd975e2d24bf
andylippitt <andylippitt@gmail.com>
2018-08-07 22:22:54 -0400

    sliders and stuff

--- Game.Engine/wwwroot/js/serverinfo.js
+++ Game.Engine/wwwroot/js/serverinfo.js
@@ -2,10 +2,35 @@
     var latencyDisplay = $('<li></li>');
     $('#ansiblelinks').append(latencyDisplay);
 
-    var BaseThrust = $('<li></li>');
-    $('#ansiblelinks').append(BaseThrust);
+
+    var labelText = "thrust";
+
+    var li = $('<li></li>');
+    var label = $('<label></label>');
+    li.append(label);
+
+    var slider = $('<div class="attribute-slider"></div>');
+    var update = function () {
+        label.text(labelText + '(' + slider.slider('value') + ') : ');
+    }
+    slider.slider({
+        min: 0,
+        max: 20,
+        slider: function (event, ui) {
+            update();
+        }
+    });
+    update();
+
+    li.append(slider);
+
+
+    $('#ansiblelinks').append(li);
+    
     var MaxBoostTime = $('<li></li>');
     $('#ansiblelinks').append(MaxBoostTime);
+
+
 
     setInterval(function () {
         var connection = window.Game.primaryConnection;
@@ -16,7 +41,7 @@
             latencyDisplay.text('ping: n/a');
 
         if (Game.Hook) {
-            BaseThrust.text('base thrust: ' + (Game.Hook.BaseThrust));
+            //BaseThrust.text('base thrust: ' + (Game.Hook.BaseThrust));
             MaxBoostTime.text('max boost time: ' + (Game.Hook.MaxBoostTime));
         }
     }, 1000);
