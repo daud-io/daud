bf7f87c412ddac040665bd91bf132b5e3604ee30
andylippitt <andylippitt@gmail.com>
2018-09-04 18:13:48 -0400

    add another backgroun parallax layer

--- Game.Engine/wwwroot/js/background.js
+++ Game.Engine/wwwroot/js/background.js
@@ -25,6 +25,9 @@
             ctx.fillStyle = this.pattern;
             ctx.translate(-x, -y)
             ctx.fillRect(-100000 + x, -100000 + y, 200000, 200000); 
+            ctx.translate(-2 * x, -2 * y)
+            ctx.globalAlpha = 0.4;
+            ctx.fillRect(-100000 + x, -100000 + y, 200000, 200000); 
             ctx.restore();
         }
     };
