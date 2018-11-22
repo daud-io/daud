fc54a927eda04a4996b6285d827439b329a44d7d
andylippitt <andylippitt@gmail.com>
2018-11-21 22:40:35 -0500

    with great power...

--- Game.Engine/wwwroot/js/panel.js
+++ Game.Engine/wwwroot/js/panel.js
@@ -1,8 +1,13 @@
 ﻿import { gui } from "./serverinfo";
 
 var pressable = true;
-document.addEventListener("keydown", function(e) {
-    if ((e.keyCode == 70 || e.which == 70) && document.body.classList.contains("alive")) {
+document.addEventListener("keydown", function (e) {
+
+    if (
+        (e.keyCode == 70 || e.which == 70)
+        && document.body.classList.contains("alive")
+        && event.getModifierState("Shift")
+    ) {
         gui.closed = !gui.closed;
         pressable = false;
     }
