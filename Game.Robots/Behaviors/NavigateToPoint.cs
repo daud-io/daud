a90132677c782c081d10322d190621d58816661a
andylippitt <andylippitt@gmail.com>
2018-12-13 08:40:26 -0500

    beginnings of navigation behavior

--- Game.Robots/Behaviors/NavigateToPoint.cs
+++ Game.Robots/Behaviors/NavigateToPoint.cs
@@ -1,5 +1,7 @@
 ﻿namespace Game.Robots.Behaviors
 {
+    using System;
+
     public class NavigateToPoint : ContextBehavior
     {
         private readonly Robot Robot;
@@ -7,6 +9,14 @@
         public NavigateToPoint(Robot robot)
         {
             this.Robot = robot;
+        }
+
+        private float CalculateDifferenceBetweenAngles(float firstAngle, float secondAngle)
+        {
+            float difference = secondAngle - firstAngle;
+            while (difference < -MathF.PI) difference += 2*MathF.PI;
+            while (difference > MathF.PI) difference -= 2 * MathF.PI;
+            return difference;
         }
 
         protected override float ScoreAngle(float angle)
