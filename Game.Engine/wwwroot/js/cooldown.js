30ed779484f074161525d121e0703677e58d3506
modderme123 <modderme123@gmail.com>
2018-12-16 10:39:20 -0800

    tweak cooldown

--- Game.Engine/wwwroot/js/cooldown.js
+++ Game.Engine/wwwroot/js/cooldown.js
@@ -12,19 +12,23 @@ export class Cooldown {
     }
     draw() {
         if (Settings.showCooldown) {
-            const cd = (this.cooldown / 255) * (this.canvas.width / 4);
+            const width = this.canvas.width / 4;
+            const cd = (this.cooldown / 255) * width;
             const ctx = this.context;
 
             ctx.save();
 
-            const pos = (this.canvas.width - cd) / 2;
+            const pos = (this.canvas.width - width) / 2;
 
             var grd = ctx.createLinearGradient(pos, 0, pos + cd, 0);
             grd.addColorStop(0, "#008888");
             grd.addColorStop(1, "#0066ff");
 
+            ctx.fillStyle = "rgba(255, 255, 255, 0.3)";
+            ctx.fillRect((this.canvas.width - width) / 2, this.canvas.height - 40, width, 7);
+
             ctx.fillStyle = grd;
-            ctx.fillRect(pos, this.canvas.height - 40, cd, 10);
+            ctx.fillRect(pos, this.canvas.height - 40, cd, 7);
 
             ctx.restore();
         }
