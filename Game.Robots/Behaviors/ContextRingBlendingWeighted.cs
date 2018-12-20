1c53ae85050654b496a36b596de6322be179f4b8
andylippitt <andylippitt@gmail.com>
2018-12-19 21:02:29 -0500

    bad commit

--- Game.Robots/Behaviors/ContextRingBlendingWeighted.cs
+++ Game.Robots/Behaviors/ContextRingBlendingWeighted.cs
@@ -16,6 +16,24 @@
         {
             var combined = new ContextRing(Robot.Steps);
 
+
+            // blur
+            foreach (var context in contexts)
+                for (var blurStep = 0; blurStep < 10; blurStep++)
+                    for (var i = 0; i < context.Weights.Length; i++)
+                    {
+                        var previousIndex = i - 1;
+                        if (previousIndex < 0)
+                            previousIndex = context.Weights.Length - 1;
+                        var prev = context.Weights[previousIndex];
+                        var next = context[(i + 1) % context.Weights.Length];
+
+                        var thisScore = context[i].score;
+                        context[i].score = context[i].score + (prev - thisScore) * .05 + (next - thisScore) * .05;
+                    }
+
+
+
             if (contexts.Any())
             {
                 for (var i = 0; i < Robot.Steps; i++)
