1af4231a00a849f5d83f33a5e0fe3fac136f7c6d
Michal R <misiak_pl@o2.pl>
2019-01-16 13:02:54 +0100

    Big log

--- Game.Engine/wwwroot/js/log.js
+++ Game.Engine/wwwroot/js/log.js
@@ -1,6 +1,8 @@
 ﻿import { Settings } from "./settings";
 
 const log = document.getElementById("log");
+const bigLog = document.getElementById("bigLog");
+
 export class Log {
     constructor() {
         this.data = [];
@@ -19,12 +21,25 @@ export class Log {
             out += `<span><b style="color:gray">${slot.time.toLocaleTimeString()}</b> ${slot.entry}</span><br>`;
         }
         log.innerHTML = out;
+		
+		var lastData = this.data[this.data.length-1]["entry"];
+		if (lastData.toLowerCase().indexOf("you killed") === 0 ) {
+			lastData = "<span style='color:#00ff00'>" + lastData + "</span>";
+		} else if (lastData.toLowerCase().indexOf("killed by") === 0 ) {
+			lastData = "<span style='color:#ff0000'>" + lastData + "</span>";
+		}
+		bigLog.innerHTML = lastData;
     }
 
     check() {
         const time = performance.now() - this.lastDisplay;
         if (time > 6000) {
             log.innerHTML = "";
+        }
+		
+		const time2 = performance.now() - this.lastDisplay;
+        if (time > 3000) {
+            bigLog.innerHTML = "";
         }
     }
 }
