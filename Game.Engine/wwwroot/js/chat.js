4e16ad97f2a77724ecfc585263dac73cf96e9369
Michal R <misiak_pl@o2.pl>
2019-01-25 12:40:55 +0100

    Chat toggle and basic functions

---
+++ Game.Engine/wwwroot/js/chat.js
@@ -0,0 +1,39 @@
+const chatToggleKey = "t";
+const chat = document.getElementById("chat");
+const messages = document.getElementById("chatMessages");
+const textbox = document.getElementById("chatTextbox");
+
+window.onkeypress = function(event) {
+	if (event.key.toLowerCase() === chatToggleKey.toLowerCase()) {
+		if (chat.style.display === "none") {
+			chat.style.display = "block";
+			textbox.focus();
+			return false;
+		} else if (textbox !== document.activeElement) {
+			chat.style.display ="none";
+		}
+	}
+}
+
+textbox.onkeypress = function(event) {
+	if (event.key === "Enter") {
+		var chat = new Chat();
+		chat.send();
+	}
+}
+
+class Chat {
+	
+	send() {
+		const txt = textbox.value;
+		if (txt !== "") {
+			textbox.value = "";
+			// some code to send message to server
+		}
+	}
+
+	update() {
+		// some code for refresching the chat
+	}
+	
+}
