6bdfb38e037700dfe277a90dcbf44c0630362dd4
andylippitt <andylippitt@gmail.com>
2019-01-29 09:11:16 -0500

    switch to release build

--- Game.Engine/build.cmd
+++ Game.Engine/build.cmd
@@ -2,9 +2,11 @@ pushd wwwroot
 rmdir /s /q dist
 call npm run build
 popd
-rmdir /s /q bin\Debug
-dotnet publish
-pushd bin\Debug\netcoreapp2.1\publish
+#rmdir /s /q bin\Debug
+rmdir /s /q bin\Release
+dotnet publish -c release
+#pushd bin\Debug\netcoreapp2.1\publish
+pushd bin\Release\netcoreapp2.1\publish
 docker build . -t andylippitt/iogame:%1
 docker push andylippitt/iogame:%1
 popd
