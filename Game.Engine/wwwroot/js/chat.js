251e07d71f7a985299464118d073cd7d242f7c00
modderme123 <modderme123@gmail.com>
2019-02-10 09:27:42 -0800

    things to say

--- Game.Engine/wwwroot/js/chat.js
+++ Game.Engine/wwwroot/js/chat.js
@@ -2,13 +2,16 @@ export var message = {
     txt: ""
 };
 var chat = document.getElementById("chat");
-var messages = ["👋", "✅", "❌", "⁉️"];
+var messages = ["✅", "❌", "⁉️", "👋", "☠️", "👑", "👈", "👉", "👆", "👇"];
+for (var i in messages) {
+    chat.innerHTML += `<tr><td>${i < 9 ? 1 + ~~i : 0}</td><td>${messages[i]}</td></tr>`;
+}
 window.addEventListener("keydown", e => {
     if (e.keyCode == 84) {
         chat.classList.add("open");
     }
     if (e.keyCode < 58 && e.keyCode > 47 && chat.classList.contains("open")) {
-        message.txt = messages[e.keyCode - 48];
+        message.txt = messages[e.keyCode - 49] || messages[messages.length - 1];
         chat.classList.remove("open");
     }
 });
