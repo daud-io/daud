3a6c9dac23ee014883f7b4f2b5ff3c7acca7a49d
modderme123 <modderme123@gmail.com>
2019-02-11 09:55:46 -0800

    untested

--- Game.Engine/wwwroot/js/chat.js
+++ Game.Engine/wwwroot/js/chat.js
@@ -9,9 +9,9 @@ for (var i in messages) {
 }
 window.addEventListener("keydown", e => {
     if (e.keyCode == 84) {
-        chat.classList.add("open");
+        chat.classList.toggle("open");
     }
-    if (e.keyCode < 58 && e.keyCode > 47 && chat.classList.contains("open")) {
+    if (e.keyCode < 58 && e.keyCode > 47 && document.body.classList.contains("alive")) {
         message.txt = messages[e.keyCode - 49] || messages[messages.length - 1];
         message.time = Date.now();
         chat.classList.remove("open");
