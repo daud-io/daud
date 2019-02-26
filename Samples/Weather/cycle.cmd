56125d5e1655afe46fda23011d9992853173949d
andylippitt <andylippitt@gmail.com>
2019-02-26 15:03:46 -0500

    Update cycle.cmd

--- Samples/Weather/cycle.cmd
+++ Samples/Weather/cycle.cmd
@@ -1,5 +1,6 @@
 SET BASEURL=https://raw.githubusercontent.com/daud-io/daud/master/Samples/Weather
 
-game world hook default --url %BASEURL%/fish-extra.json
-pause 5
-game world hook default --url %BASEURL%/fish-standard.json
+call game world hook default --url %BASEURL%/fish-extra.json
+sleep 5
+call game world hook default --url %BASEURL%/fish-standard.json
+sleep 5
