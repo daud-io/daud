eaf3ada232dec4f6347d11ff137492fba4d050db
andylippitt <andylippitt@gmail.com>
2019-02-26 19:45:58 -0500

    more weather

--- Samples/Weather/cycle.cmd
+++ Samples/Weather/cycle.cmd
@@ -1,6 +1,37 @@
-call apply fish-extra
-call apply fish-fast
-sleep 60
+for /l %%x in (1, 1, 100000) do (
+   echo %%x
 
-call apply fish-standard
-sleep 60
+	call apply fish-fast
+	sleep 300
+	call apply fish-extra
+	sleep 300
+	call apply fish-standard
+	sleep 1
+
+	call apply dauds-extra
+	sleep 300
+	call apply dauds-standard
+	sleep 1
+
+	call apply shields-none
+	sleep 300
+	call apply shields-standard
+	sleep 300
+	call apply shields-extra
+	sleep 300
+	call apply shields-standard
+	sleep 300
+
+	call apply obstacles-fast
+	sleep 300
+	call apply obstacles-extra
+	sleep 300
+	call apply obstacles-standard
+	sleep 300
+
+	call apply robotguns-extra
+	sleep 300
+	call apply robotguns-standard
+	sleep 300
+
+)
---
+++ Samples/Weather/reset.cmd
@@ -0,0 +1,6 @@
+
+call apply fish-standard
+call apply dauds-standard
+call apply shields-standard
+call apply obstacles-standard
+call apply robotguns-standard
