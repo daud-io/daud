71ca74bd5e604b6c5dcde66625078f85c40937c9
Cole Kissane <coler706@gmail.com>
2019-03-16 16:17:29 -0700

    The SCSSilmarillion

--- Game.Engine/wwwroot/js/parser/parseTheme.js
+++ Game.Engine/wwwroot/js/parser/parseTheme.js
@@ -2,128 +2,9 @@ const antlr4 = require("antlr4");
 const CSSselect = require("css-select");
 var ScssLexer = require("./ScssLexer").ScssLexer;
 var ScssParser = require("./ScssParser").ScssParser;
-var input = `#ship {
-   textures: 'ship_gray';
- }
- #ship.gray {
-   textures: 'ship_gray';
- }
- #ship.bot {
-   textures: 'ship0';
- }
- #ship.secret {
-   textures: 'ship_secret';
- }
- #ship.zed {
-   textures: 'ship_zed';
- }
- #ship.cyan {
-   textures: 'ship_cyan';
- }
- #ship.boost {
-   textures: inherit, 'thruster_cyan';
- }
- #ship.blue {
-   textures: 'ship_blue';
- }
- #ship.blue.boost {
-   textures: inherit, 'thruster_blue';
- }
- #ship.green {
-   textures: 'ship_green';
- }
- #ship.green.boost {
-   textures: inherit, 'thruster_green';
- }
- #ship.orange {
-   textures: 'ship_orange';
- }
- #ship.orange.boost {
-   textures: inherit, 'thruster_orange';
- }
- #ship.pink {
-   textures: 'ship_pink';
- }
- #ship.pink.boost {
-   textures: inherit, 'thruster_pink';
- }
- #ship.red {
-   textures: 'ship_red';
- }
- #ship.red.boost {
-   textures: inherit, 'thruster_red';
- }
- #ship.yellow {
-   textures: 'ship_yellow';
- }
- #ship.yellow.boost {
-   textures: inherit, 'thruster_yellow';
- }
- #ship.offenseupgrade {
-   textures: inherit, 'offenseupgrade';
- }
- #ship.defenseupgrade {
-   textures: inherit, 'defenseupgrade';
- }
- #ship.invulnerable {
-   textures: inherit, 'invulnerable';
- }
- #ship.shield {
-   textures: inherit, 'shield';
- }
- #bullet {
-   textures: 'bullet';
- }
- #bullet.cyan {
-   textures: 'bullet_cyan';
- }
- #bullet.blue {
-   textures: 'bullet_blue';
- }
- #bullet.green {
-   textures: 'bullet_green';
- }
- #bullet.orange {
-   textures: 'bullet_orange';
- }
- #bullet.pink {
-   textures: 'bullet_pink';
- }
- #bullet.red {
-   textures: 'bullet_red';
- }
- #bullet.yellow {
-   textures: 'bullet_yellow';
- }
- #obstacle {
-   textures: 'obstacle';
- }
- #wormhole {
-   textures: 'wormhole';
- }
- #seeker {
-   textures: 'seeker';
- }
- #pickup.seeker {
-   textures: 'seeker_pickup';
- }
- #pickup.shield {
-   textures: 'shield_pickup';
- }
- #ctf_base {
-   textures: 'ctf_base';
- }
- #ctf_flag.blue {
-   textures: 'ctf_flag_blue';
- }
- #ctf_flag.red {
-   textures: 'ctf_flag_red';
- }
- #map {
-   textures: 'map';
- }
-  `;
+var sass = require('sass');
 
+var input = sass.renderSync({file: "../models/spriteModeMap.scss"}).css.toString('utf8'); 
 function parseCssIntoRules(css) {
    var chars = new antlr4.InputStream(css);
    var lexer = new ScssLexer(chars);
@@ -169,14 +50,11 @@ function selectorMatches(selector, selectProps) {
 }
 
 
-//var queryProperties({id: "ship", class: ".cyan", modifiers: [ "[boost=1]" ] });
 function queryProperties(element) {
    var res = {};
    for (var i = 0; i < ruleList.length; i++) {
       if (selectorMatches(ruleList[i].selector, element)) {
-         //console.log("MATCH",ruleList[i].selector,element);
          for (var p in ruleList[i].obj) {
-
             if (res[p] == undefined) {
                res[p] = "''";
             }
@@ -188,6 +66,7 @@ function queryProperties(element) {
 }
 
 function getShipProperties(ship, more) {
-   return queryProperties({ id: "ship", class: ship + " " + more.join(" "), })
+   return queryProperties({ element:"ship", class: ship + " " + more.join(" "), })
 }
 console.log(getShipProperties("cyan", ["boost", "defenseupgrade"]))
+console.log(queryProperties({ element: "bg"}))
