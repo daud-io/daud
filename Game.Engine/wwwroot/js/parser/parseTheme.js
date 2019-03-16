314e78c8ebf4a4f5695487193494d0e6342124e4
Cole Kissane <coler706@gmail.com>
2019-03-16 15:46:02 -0700

    The Return of the Theme

--- Game.Engine/wwwroot/js/parser/parseTheme.js
+++ Game.Engine/wwwroot/js/parser/parseTheme.js
@@ -124,49 +124,27 @@ var input = `#ship {
  }
   `;
 
-//its fish fault lol i removed #fish and works again
 function parseCssIntoRules(css) {
    var chars = new antlr4.InputStream(css);
    var lexer = new ScssLexer(chars);
    var tokens = new antlr4.CommonTokenStream(lexer);
    var parser = new ScssParser(tokens);
    var tree = parser.stylesheet();
-   // console.log(parser);
-   // console.log(tree.statement(0).ruleset().selectors().selector(0).element(0).identifier().getText())//prints test!
-   // console.log(tree.statement(0).ruleset().block().property(0).getText())//prints background:"green"
-   // console.log(tree.statement(0).ruleset().block().property(0).identifier().getText())//prints background!
-   // console.log(tree.statement(0).ruleset().block().property(0).values().commandStatement(0).getText())//prints "green"
-
-   // the way we are supposed to do this is to make a tree traverser that generates what we want
-   // yea, I'm worried that's a huge task ... that seems to have been knocked out in 30 minutes.
-   // rather w/
-   //https://github.com/antlr/antlr4/blob/master/doc/javascript-target.md#how-do-i-create-and-run-a-visitor
 
    var ruleList = [];
    function addRulesFromStatement(statement, rules) {
       var selectors = statement.ruleset().selectors();
       var block = statement.ruleset().block();
 
-      // consider the final .values().getText() in the following line. I think it's crushing an array of your border selector hmm
-
-      //fixed
       var blockProps = block.property().map((x, i) => [block.property(i).identifier().getText(), block.property(i).values().children.map(x => x.children ? x.children.map(y => y.getText()).join(" ") : x.getText()).join(" ")]);//this
       var blockOBJ = {};
-      //lets now make the get prop method
-      //https://www.npmjs.com/package/css-select
-      //this will let us test selectors 
+
       for (var i = 0; i < blockProps.length; i++) {
          blockOBJ[blockProps[i][0]] = blockProps[i][1];
-
       }
       for (var i = 0; i < selectors.children.length; i++) {
          rules.push({ selector: selectors.selector(i).getText(), obj: blockOBJ });
       }
-
-      //we need it to parse selector now
-      //(it gives obj:{background:'"green"',color:'red'})
-
-
    }
 
    //nested select is still broken but should be an easy fix ( or could just compile to css)
@@ -176,8 +154,6 @@ function parseCssIntoRules(css) {
    return ruleList;
 }
 var ruleList = parseCssIntoRules(input);
-// console.log("PARSED RULES: ")
-// console.log(ruleList);
 
 function selectorMatches(selector, selectProps) {
    var thing = {
@@ -193,7 +169,7 @@ function selectorMatches(selector, selectProps) {
 }
 
 
-//var queryProperty({id: "ship", class: ".cyan", modifiers: [ "[boost=1]" ] });
+//var queryProperties({id: "ship", class: ".cyan", modifiers: [ "[boost=1]" ] });
 function queryProperties(element) {
    var res = {};
    for (var i = 0; i < ruleList.length; i++) {
