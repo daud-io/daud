b879a6fc0006ed921a84571a780268eebc907645
modderme123 <modderme123@gmail.com>
2019-04-07 08:30:57 -0700

    Compact

--- Game.Engine/Core/Fleet.cs
+++ Game.Engine/Core/Fleet.cs
@@ -77,7 +77,7 @@
                 switch (Owner.ShipSprite)
                 {
                     case Sprites.ship_cyan: return Sprites.bullet_cyan;
-					case Sprites.ship_blue: return Sprites.bullet_blue;
+                    case Sprites.ship_blue: return Sprites.bullet_blue;
                     case Sprites.ship_green: return Sprites.bullet_green;
                     case Sprites.ship_orange: return Sprites.bullet_orange;
                     case Sprites.ship_pink: return Sprites.bullet_pink;
@@ -257,41 +257,12 @@
 
                 BoostUntil = World.Time + World.Hook.BoostDuration;
                 isBoostInitial = true;
-                
-                List<int> Sorter = new int[] {0}.ToList();;
-                for (int i = 1; i < Ships.Count; i++)
-                {
-                    bool added = false;
-                    var ship = Ships[i];
-                    var shipTargetVector = FleetCenter + AimTarget - ship.Position;
-                    var shipDistance = shipTargetVector.X * shipTargetVector.X + shipTargetVector.Y * shipTargetVector.Y;
-                    for (int j = 0; j < Sorter.Count; j++)
-                    {
-                        var ship2 = Ships[Sorter[j]];
-                        var shipTargetVector2 = FleetCenter + AimTarget - ship2.Position;
-                        var shipDistance2 = shipTargetVector2.X * shipTargetVector2.X + shipTargetVector2.Y * shipTargetVector2.Y;
-                        if (shipDistance >= shipDistance2)
-                        {
-                            Sorter.Insert(j, i);
-                            added = true;
-                            break;
-                        }
-                    }
-                    if (!added)
-                    {
-                        Sorter.Add(i);
-                    }
-                }
-                
+
                 var shipLoss = (int)MathF.Floor(Ships.Count / 2);
-                for (int i = 0; i < shipLoss; i++)
+                var Sorter = Ships.OrderByDescending((ship) => Vector2.DistanceSquared(FleetCenter + AimTarget, ship.Position)).Take(shipLoss);
+                foreach (Ship ship in Sorter)
                 {
-                    try {
-                        var ship = Ships[Sorter[i]];
-                        AbandonShip(ship);
-                    }
-                    catch {
-                    }
+                    AbandonShip(ship);
                 }
             }
 
