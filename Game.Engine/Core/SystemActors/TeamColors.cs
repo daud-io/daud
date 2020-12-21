c649c0e72c3083df9115b45d3de0641e58b03abd
Andy Lippitt <andylippitt@gmail.com>
2020-12-21 18:42:26 -0500

    make teammode hook change work.. a bit better

--- Game.Engine/Core/SystemActors/TeamColors.cs
+++ Game.Engine/Core/SystemActors/TeamColors.cs
@@ -13,8 +13,18 @@
                         case API.Common.Sprites.ship_green:
                         case API.Common.Sprites.ship_yellow:
                         case API.Common.Sprites.ship_secret:
+
                             player.ShipSprite = API.Common.Sprites.ship_cyan;
                             player.Color = "cyan";
+
+                            if (player.Fleet?.Ships?.Count > 0)
+                                foreach (var ship in player.Fleet.Ships)
+                                {
+                                    ship.Color = player.Color;
+                                    ship.Sprite = player.ShipSprite;
+                                }
+
+                            
                             break;
                         case API.Common.Sprites.ship_orange:
                         case API.Common.Sprites.ship_pink:
@@ -22,9 +32,19 @@
                         case API.Common.Sprites.ship_zed:
                             player.ShipSprite = API.Common.Sprites.ship_red;
                             player.Color = "red";
+
+                            if (player.Fleet?.Ships?.Count > 0) 
+                                foreach (var ship in player.Fleet.Ships)
+                                {
+                                    ship.Color = player.Color;
+                                    ship.Sprite = player.ShipSprite;
+                                }
+
                             break;
                     }
             }
         }
+
+        
     }
 }
