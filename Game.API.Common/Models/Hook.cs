d8e8a0db2186ad39b5579a04f7355d6316cca251
Michal R <misiak_pl@o2.pl>
2021-01-22 12:42:21 +0100

    Physics

--- Game.API.Common/Models/Hook.cs
+++ Game.API.Common/Models/Hook.cs
@@ -49,7 +49,7 @@
                         5.525f, 5.511f, 5.497f, 5.484f, 5.470f, 5.457f, 5.444f, 5.432f, 5.419f, 5.407f, // 81 - 90 size
                         5.394f, 5.382f, 5.370f, 5.359f, 5.347f, 5.335f, 5.324f, 5.312f, 5.300f, 5.289f // 91 - 100 size
                     },
-                    BaseThrustConverter = 0.0028f,
+                    BaseThrustConverter = 0.0025f,
 
                     Drag = 0.87f,
                     DragBoost = 0.96f,
@@ -58,8 +58,8 @@
                     BoomDrag = 0.92f,
                     BoomLife = 500,
 
-                    BoostThrust = 0.013f,
-                    BoostThrust2 = 0.115f,
+                    BoostThrust = 0.01f,
+                    BoostThrust2 = 0.11f,
 
                     BoostCooldownTimeM = 14.0f,
                     BoostCooldownTimeB = 1080.0f,
