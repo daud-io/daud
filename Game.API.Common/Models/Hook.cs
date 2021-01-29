a71cf574a645db01a0dc167c7859d9815dbbd8e0
Michal R <misiak_pl@o2.pl>
2021-01-29 11:07:06 +0100

    Physics improved

--- Game.API.Common/Models/Hook.cs
+++ Game.API.Common/Models/Hook.cs
@@ -49,24 +49,24 @@
                         5.525f, 5.511f, 5.497f, 5.484f, 5.470f, 5.457f, 5.444f, 5.432f, 5.419f, 5.407f, // 81 - 90 size
                         5.394f, 5.382f, 5.370f, 5.359f, 5.347f, 5.335f, 5.324f, 5.312f, 5.300f, 5.289f // 91 - 100 size
                     },
-                    BaseThrustConverter = 0.002f,
+                    BaseThrustConverter = 0.0025f,
 
-                    Drag = 0.9f,
-                    DragBoost = 0.9f,
+                    Drag = 0.87f,
+                    DragBoost = 0.87f,
                     DragAbandoned = 0.97f,
 
                     BoomDrag = 0.92f,
                     BoomLife = 500,
 
-                    BoostThrust = 0.025f,
-                    BoostThrust2 = 0.065f,
+                    BoostThrust = 0.034f,
+                    BoostThrust2 = 0.073f,
 
                     BoostCooldownTimeM = 14.0f,
                     BoostCooldownTimeB = 1080.0f,
                     ShotCooldownTimeShark = 300,
 
                     BoostSpeed = 0f,
-                    BoostDuration2 = 300,
+                    BoostDuration2 = 330,
                     BoostDuration = 1000,
 
                     AbandonBuffer = 120,
