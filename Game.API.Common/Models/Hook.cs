51c34d18b56e76a88bd5ef2be77152a6aab2d8e7
Michal R <misiak_pl@o2.pl>
2021-02-23 14:25:02 +0100

    Improved boost

--- Game.API.Common/Models/Hook.cs
+++ Game.API.Common/Models/Hook.cs
@@ -61,8 +61,8 @@
                     BoomDrag = 0.92f,
                     BoomLife = 500,
 
-                    BoostThrust = 0.045f,
-                    BoostThrust2 = 0.035f,
+                    BoostThrust = 0.05f,
+                    BoostThrust2 = 0.05f,
 
                     BoostCooldownTimeM = 14.0f,
                     BoostCooldownTimeB = 1080.0f,
