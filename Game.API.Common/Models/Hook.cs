2e5e4b4f34b8fb21d4551858418e0e1ad60a165b
Michal R <misiak_pl@o2.pl>
2021-08-27 12:44:52 +0200

    Update Hook.cs

--- Game.API.Common/Models/Hook.cs
+++ Game.API.Common/Models/Hook.cs
@@ -52,7 +52,7 @@
                         5.525f, 5.511f, 5.497f, 5.484f, 5.470f, 5.457f, 5.444f, 5.432f, 5.419f, 5.407f, // 81 - 90 size
                         5.394f, 5.382f, 5.370f, 5.359f, 5.347f, 5.335f, 5.324f, 5.312f, 5.300f, 5.289f // 91 - 100 size
                     },
-                    BaseThrustConverter = 0.0022f,
+                    BaseThrustConverter = 0.0024f,
 
                     Drag = 1f,
                     DragBoost = 1f,
